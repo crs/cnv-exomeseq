a790b83c26ab04c9c802f07660b65c5ec399d339
Christian Spaniol <mail@christian-spaniol.de>
2018-07-05 11:46:30 +0200

    snake cleanup, readme update

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Req.:
 - samtools
 - CNVnator
 - Python
+
+
+* v0.1 
+	- Single sample pipeline, folder tracking not yet implemented but intended.
+	- Works on paired-end data (R1, R2) and two lanes for each sample
--- Snakefile.py
+++ Snakefile.py
@@ -1,59 +1,64 @@
-### align exome seq
+### align exome seq files and call with cnvnator
 
 THREADS=64
 JAVA='~/jdk1.8.0_92/bin/java '
 CNVNATOR='bin/CNVnator-master/cnvnator '
 BIN_SIZE=100
 rule all:
-	#input: 'bam/FTD_P1_E02-35794035/FTD-P1-E02_S15_L003_L004.bam'
-	#input: 'rawcnv/FTD_P1_E02-35794035/FTD-P1-E02_S15_L003_L004.rawcnv'
-	#input: 'root/FTD_P1_E02-35794035/FTD-P1-E02_S15_L003_L004.root'
-	#input: 'logs/FTD_P1_E02-35794035/statistics.FTD-P1-E02_S15_L003_L004.log', 'logs/FTD_P1_E02-35794035/histogram.FTD-P1-E02_S15_L003_L004.log','logs/FTD_P1_E02-35794035/partition.FTD-P1-E02_S15_L003_L004.log'
 	#input: 'cnvs/FTD_P1_E02-35794035/FTD-P1-E02_S15_L003_L004.cnv'
-	input: 'cnvs/FTD_P1_F04-35810021/FTD-P1-F04_S16_L003_L004.cnv'
-	#input: 'aligned/FTD_P1_E02-35794035/FTD-P1-E02_S15_L003_R1_001.sai'
+	input: 'cnvs/FTD_P1_F04-35810021/FTD-P1-F04_S16_L003_L004.cnv'	
 
 rule cnv_calling:
+	""" call the cnvs with CNVnator """
 	input: log='logs/{sample}/partition.{file}_L{laneA}_L{laneB}.log', 		
 	output: 'cnvs/{sample}/{file}_L{laneA}_L{laneB}.cnv'
 	params: bin_size=BIN_SIZE, root='root/{sample}/{file}_L{laneA}_L{laneB}.root'
 	shell: CNVNATOR + "-root {params.root} -call {params.bin_size} > {output}"
 	
 rule rd_signal_partitioning:
+	""" partitioning step, this takes a while """
 	input:  log='logs/{sample}/statistics.{file}_L{laneA}_L{laneB}.log'
 	output: log='logs/{sample}/partition.{file}_L{laneA}_L{laneB}.log'
 	params: bin_size=BIN_SIZE, root='root/{sample}/{file}_L{laneA}_L{laneB}.root',
 	shell: CNVNATOR + "-root {params.root} -partition {params.bin_size} > {output.log}"
 
 rule statistics:
+	""" create statistics on the root"""
 	input: log='logs/{sample}/histogram.{file}_L{laneA}_L{laneB}.log'	
 	output: log='logs/{sample}/statistics.{file}_L{laneA}_L{laneB}.log'
 	params: bin_size=BIN_SIZE, root='root/{sample}/{file}_L{laneA}_L{laneB}.root'
 	shell: CNVNATOR + "-root {params.root} -genome hg18 -stat {params.bin_size} > {output.log}"
 
 rule generate_histogram:
+	""" create histogram, as the original files are modified, further requirements for snakemake are the logs from streaming std into files """
 	input: log='logs/{sample}/extraction.{file}_L{laneA}_L{laneB}.log', reference="indexed/chr"
 	output: log='logs/{sample}/histogram.{file}_L{laneA}_L{laneB}.log'
 	params: bin_size=BIN_SIZE, root='root/{sample}/{file}_L{laneA}_L{laneB}.root'
 	shell: CNVNATOR + "-root {params.root} -his {params.bin_size} -d {input.reference} > {output.log}"
 	
 rule extractReadMapping:
+	""" use root to generate a tree """
 	input: bam='bam/{sample}/{file}_L{laneA}_L{laneB}.bam'
 	output: root='root/{sample}/{file}_L{laneA}_L{laneB}.root', log='logs/{sample}/extraction.{file}_L{laneA}_L{laneB}.log'
 	shell: CNVNATOR + "-root {output.root} -tree {input.bam} -unique > {output.log}"
 	
+### still incomplete, have to fix the subpipeline which is limited to hg19 and hg38
 rule runPennCNV:
+	"""use PennCNV-seq to create intensity files and call cnvs"""
 	input: bam='bam/{sample}/{file}_L{laneA}_L{laneB}.bam', reference='indexed/hg18.fa'
 	output: 'rawcnv/{sample}/{file}_L{laneA}_L{laneB}.rawcnv'
 	shell: "./bin/PennCNV-Seq-master/penncnv-seq-hg18.sh ~/CNV/PennCNV-1.0.4 ~/bin/PennCNV-Seq-master/reference hg18 EUR {input.reference} {input.bam}; touch {output}"
 
 rule convert_and_sort_sam_to_bam:
+	""" samtools sam to bam conversion; piping into a sorted bam """
 	input: sam='sam/{sample}/{file}_L{laneA}_L{laneB}.sam'
 	output: bam='bam/{sample}/{file}_L{laneA}_L{laneB}.bam', bai='bam/{sample}/{file}_L{laneA}_L{laneB}.bai'
 	params: bam='bam/{sample}/{file}_L{laneA}_L{laneB}'
 	shell: 'samtools view -bS {input.sam} | samtools sort - {params.bam}'
-	
+
+### Defect, doesnt work with the output created by bwa	
 #rule convert_sam_to_bam:
+#	""" use picard to create bam file from sam"""
 #	input: sam='sam/{sample}/{file}_L{laneA}_L{laneB}.sam'
 #	output: bam='bam/{sample}/{file}_L{laneA}_L{laneB}.bam'
 #	shell: JAVA + """-Xmx4g -Djava.io.tmpdir=/tmp \
@@ -65,6 +70,7 @@ rule convert_and_sort_sam_to_bam:
 #					CREATE_INDEX=true"""
 	
 rule pairedEnd:
+	""" make a paired end sam file from forward and reverse strands"""
 	input: fwd_sai='aligned/{sample}/{file}_L{laneA}_L{laneB}_R1.sai', rev_sai='aligned/{sample}/{file}_L{laneA}_L{laneB}_R2.sai', fwd_fastq='merged/{sample}/{file}_L{laneA}_L{laneB}_R1.fastq', rev_fastq='merged/{sample}/{file}_L{laneA}_L{laneB}_R2.fastq'
 	output: sam='sam/{sample}/{file}_L{laneA}_L{laneB}.sam'
 	shell: """bwa sampe -f {output.sam} -r '@RG\tID:{wildcards.sample}_{wildcards.file}_L{wildcards.laneA}_L{wildcards.laneB}\tLB:{wildcards.sample}_{wildcards.file}_L{wildcards.laneA}_L{wildcards.laneB}\tSM:{wildcards.sample}_{wildcards.file}_L{wildcards.laneA}_L{wildcards.laneB}\tPL:ILLUMINA' indexed/hg18 {input.fwd_sai} {input.rev_sai} {input.fwd_fastq} {input.rev_fastq}"""
@@ -78,6 +84,7 @@ rule alignSamples:
 	shell: 'bwa aln -t {threads} -f {output.sai} {params.index} {input.fastq}'
 
 rule merge:
+	""" Merge the lanes into single fastq """
 	input: laneA='fastq/{sample}/{file}_L{laneA}_R{R}_001.fastq', laneB='fastq/{sample}/{file}_L{laneB}_R{R}_001.fastq'
 	output: 'merged/{sample}/{file}_L{laneA}_L{laneB}_R{R}.fastq'
 	shell: 'cat {input.laneA} {input.laneB} > {output}'
