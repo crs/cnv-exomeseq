bdbd39baa48501f28dfca16057002a5a2918792a
Christian Spaniol <mail@christian-spaniol.de>
2018-07-11 13:33:10 +0200

    Added region filtering for telomers/centromers. Added bin size expansion to compute cnvs for multiple bin sizes

--- Snakefile.py
+++ Snakefile.py
@@ -5,23 +5,77 @@ JAVA='~/jdk1.8.0_92/bin/java '
 CNVNATOR='bin/CNVnator-master/cnvnator '
 BWA='~/bin/bwa '
 SAMTOOLS='bin/CNVnator-master/samtools/samtools '
+GAPFILE='centromers.telomers.txt'
 
-MIN_CNV_LENGTH=100000
+MIN_CNV_LENGTH=50000
 PVAL_THRESHOLD=0.05
 QVAL_THRESHOLD=0.5
-BIN_SIZE=1000
+BIN_SIZE=[20, 100, 300]
 
 rule all:
 	#input: 'cnvs/FTD_P1_E02-35794035/FTD-P1-E02_S15_L003_L004.cnv'
 	#input: 'cnvs/FTD_P1_F04-35810021/FTD-P1-F04_S16_L003_L004.cnv'	
-	input: 'filtered_cnvs/FTD_P1_E09-35796034/FTD-P1-E09_S7_L001_L002_bin' + str(BIN_SIZE) + '.cnv'	
+	input: expand('filtered_cnvs/FTD_P1_E09-35796034/FTD-P1-E09_S7_L001_L002_bin{bin_size}_length' + str(MIN_CNV_LENGTH) +'.cnv', bin_size=BIN_SIZE)
 
+	
 rule filter_CNVs:
+	""" filter cnvs for: cnv length, e-val threshold, and q-value. see >>> cnv calling at https://github.com/abyzovlab/CNVnator/blob/master/README"""
+	input: 'gapsRemoved/{sample}/{file}_L{laneA}_L{laneB}_bin{bin_size}.cnv'
+	output: 'filtered_cnvs/{sample}/{file}_L{laneA}_L{laneB}_bin{bin_size}_length{length}.cnv'
+	params: pval=PVAL_THRESHOLD, qval=QVAL_THRESHOLD
+	shell: """TOTAL=`wc -l < {input}`;
+		awk '{{ if ($9 <= 0 && $9 < {params.qval} && $3 >= {wildcards.length} && $5 <= {params.pval} && $6 <= {params.pval} && $7 < {params.pval} && $8 < {params.pval}) print; }}' {input} | sort -g -k5,6 > {output}; FILTERED=`wc -l < {output}`; echo $FILTERED / $TOTAL cnvs left"""
+
+rule removeGaps:
 	input: 'cnvs/{sample}/{file}_L{laneA}_L{laneB}_bin{bin_size}.cnv'
-	output: 'filtered_cnvs/{sample}/{file}_L{laneA}_L{laneB}_bin{bin_size}.cnv'
-	params: length=MIN_CNV_LENGTH, pval=PVAL_THRESHOLD, qval=QVAL_THRESHOLD
-	shell: """
-		awk '{{ if (0 <= $9 && $9 < {params.qval} && $3 >= {params.length} && $5 <= {params.pval} && $6 <= {params.pval} && $7 < {params.pval} && $8 < {params.pval}) print; }}' {input} | sort -g -k5,6 > {output}"""
+	output: temp('gapsRemoved/{sample}/{file}_L{laneA}_L{laneB}_bin{bin_size}.cnv')
+	params: gapfile=GAPFILE
+	run: 	
+		def getTuple(loc):
+			loc = loc.strip()
+			[chr, interval] = loc.split(':')
+			[start, end] = interval.split('-')
+			return chr, start, end
+		
+		file = open(GAPFILE, 'r')
+		map = {}
+		for line in file.readlines():
+			chr, start, end = getTuple(line)
+			if chr not in map:
+				map[chr] = []
+			map[chr].append([start,end])
+		
+		file.close()
+		
+		lines=0
+		totalGapCalls=0
+		written=0
+		file = open(input[0], 'r')
+		outfile = open(output[0], 'w')
+		for line in file.readlines():
+			location = line.split('\t')[1]
+			chr, start, end = getTuple(location)
+			
+			if chr in map:
+				gapFlag = False
+				for locs in map[chr]:
+					gapStart = locs[0]
+					gapEnd = locs[1]
+					if (start <= gapEnd and not end <= gapStart) or (end >= gapEnd and not start >= gapStart): 
+						#print(chr, start, end, 'locus in gapped region')
+						gapFlag = True					
+				if not gapFlag:
+					outfile.write(line)
+					written += 1
+				else:
+					totalGapCalls += 1
+			lines += 1
+		file.close()
+		outfile.close()
+		
+		print("%d from %d calls removed due to centromere/telomere overlaps. %d cnv calls remaining" % (totalGapCalls, lines, written))
+					
+	
 	
 rule cnv_calling:
 	""" call the cnvs with CNVnator """
